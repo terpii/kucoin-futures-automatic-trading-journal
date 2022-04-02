dc411365d530a8846848b546d667cd7d078dfbdc
terpii <56959236+terpii@users.noreply.github.com>
2022-04-02 12:54:25 +0100

    Added api key stuff

--- kucoin-futures-automatic-trading-journal.py
+++ kucoin-futures-automatic-trading-journal.py
@@ -1,3 +1,17 @@
 import kucoin_futures
 
-print("Hello world")
+print('Successfully started kucoin futures trading journal\n')
+
+def open_file(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content][0]#Remove \n and stuff just incase
+    return content
+
+#Load api keys
+api_key = open_file('api_key')
+api_key_secret = open_file('api_key_secret')
+api_password = open_file('api_password')
+
+print(f'Loaded api keys:\napi_key: {api_key}\napi_key_priv: {api_key_secret}')
+print(f'Loaded api password: {api_password}')
